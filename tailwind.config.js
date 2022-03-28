587e71ba68de6197fd05f9ec36a2aac4063df7df
moyahi <asahi0i0i0i@gmail.com>
2022-03-28 16:59:18 +0900

    [update]トップページ調整 #7
    ・Googleアイコン導入
    ・大部分のスタイル調整

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,6 @@ module.exports = {
     container: {
       center: true, //.container要素を自動で中央にする
       padding: { //ブレークポイントごとに水平パディングを設定する
-        DEFAULT: '.5rem',
-        sm: '.5rem', // SP画面：640px 以上から影響
-        md: '.5rem', // PC画面：768px 以上から影響
-        lg: '.5rem', // 大画面：1024px 以上から影響
       },
     },
   },
